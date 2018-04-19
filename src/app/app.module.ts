694613db422841214694953c4e445da35ffd4d00
Ksenia Abakumova <k.s.abakumova@gmail.com>
2018-04-19 15:09:13 +0500

    + | components structure in progress |

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     NgxChartsModule,
+    AppRoutingModule,
     LeafletModule.forRoot()
   ],
   providers: [],
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,214 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3-shape';
+import * as L from 'leaflet';
+import { MapComponent } from '@yaga/leaflet-ng2';
+import HeatmapOverlay = require('leaflet-heatmap/leaflet-heatmap');
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  // Charts
+  public view: any[] = [450, 120];
+  public unitsData = [
+    {
+      'name': 'Unit price',
+      'series': [
+        {
+          'name': '01/01',
+          'value': 100
+        },
+        {
+          'name': '01/02',
+          'value': 110
+        },
+        {
+          'name': '01/03',
+          'value': 150
+        },
+        {
+          'name': '01/04',
+          'value': 150
+        },
+        {
+          'name': '01/05',
+          'value': 155
+        },
+        {
+          'name': '01/06',
+          'value': 180
+        },
+      ]
+    },
+    {
+      'name': 'Storage price',
+      'series': [
+        {
+          'name': '01/01',
+          'value': 700
+        },
+        {
+          'name': '01/02',
+          'value': 680
+        },
+        {
+          'name': '01/03',
+          'value': 710
+        },
+        {
+          'name': '01/04',
+          'value': 730
+        },
+        {
+          'name': '01/05',
+          'value': 740
+        },
+        {
+          'name': '01/06',
+          'value': 800
+        },
+      ]
+    }
+  ];
+  public priceData = [
+    {
+      'name': 'Token price',
+      'series': [
+        {
+          'name': '01/01',
+          'value': 100
+        },
+        {
+          'name': '01/02',
+          'value': 110
+        },
+        {
+          'name': '01/03',
+          'value': 150
+        },
+        {
+          'name': '01/04',
+          'value': 150
+        },
+        {
+          'name': '01/05',
+          'value': 155
+        },
+        {
+          'name': '01/06',
+          'value': 180
+        },
+      ]
+    }
+  ];
+
+  // Charts options
+  public showXAxis = true;
+  public showYAxis = true;
+  public gradient = true;
+  public showLegend = true;
+  public showXAxisLabel = false;
+  public xAxisLabel = 'Month';
+  public showYAxisLabel = false;
+  public yAxisLabel = 'Price';
+
+  public colorScheme = {
+    domain: ['#f993ab', '#ffc8a7']
+  };
+  colorSchemePrice = {
+    domain: ['#17f9be']
+  };
+  public autoScale = true;
+  public curve = d3.curveNatural;
+
+
+  // Map
+  public OSM_TILE_LAYER_URL = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png';
+  public options = {
+    layers: [
+      L.tileLayer(this.OSM_TILE_LAYER_URL,
+        {
+          subdomains: 'abcd',
+          maxZoom: 5
+        })
+    ],
+    zoom: 0,
+    center: L.latLng(46.879966, -121.726909)
+  };
+  public layers = [
+    L.circle([46.95, -102], { radius: 200000, color: '#25dfec' }),
+    L.circle([46.95, -102], { radius: 200000, color: '#25dfec' }),
+    L.circle([46.95, -102], { radius: 200000, color: '#25dfec' }),
+    L.circle([46.95, -102], { radius: 200000, color: '#25dfec' }),
+    L.circle([58.95, -182], { radius: 200000, color: '#25dfec' }),
+    L.circle([66.95, -122], { radius: 200000, color: '#25dfec' }),
+    L.circle([66.95, -222], { radius: 200000, color: '#25dfec' }),
+    L.circle([36.95, -352], { radius: 200000, color: '#25dfec' }),
+    L.circle([36.95, -352], { radius: 200000, color: '#25dfec' })
+  ];
+
+  // Heatmap
+  public options2 = {
+    layers: [
+      L.tileLayer(this.OSM_TILE_LAYER_URL,
+        {
+          subdomains: 'abcd',
+          maxZoom: 5
+        })
+    ],
+    zoom: 0,
+    center: L.latLng(46.879966, -121.726909)
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public onMapReady(map: L.Map): void {
+    // Do stuff with map
+    const testData = {
+      max: 10,
+      min: 1,
+      data: [
+        { lat: 46.95, lng: -102, count: 4 },
+        { lat: 58.95, lng: -182, count: 1 },
+        { lat: 66.95, lng: -122, count: 1 },
+        { lat: 66.95, lng: -222, count: 1 },
+        { lat: 36.95, lng: -352, count: 2 }
+      ]
+    };
+    const baseLayer = L.tileLayer(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18
+      }
+    );
+
+    const cfg = {
+      // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+      // if scaleRadius is false it will be the constant radius used in pixels
+      'radius': 15,
+      'maxOpacity': .6,
+      // scales the radius based on map zoom
+      'scaleRadius': true,
+      // if set to false the heatmap uses the global maximum for colorization
+      // if activated: uses the data maximum within the current map boundaries
+      //   (there will always be a red spot with useLocalExtremas true)
+      'useLocalExtrema': true,
+      // which field name in your data represents the latitude - default 'lat'
+      latField: 'lat',
+      // which field name in your data represents the longitude - default 'lng'
+      lngField: 'lng',
+      // which field name in your data represents the data value - default 'value'
+      valueField: 'count'
+    };
+
+
+    const heatmapLayer = new HeatmapOverlay(cfg);
+
+    heatmapLayer.setData(testData);
+    heatmapLayer.onAdd(map);
+  }
+
+}
