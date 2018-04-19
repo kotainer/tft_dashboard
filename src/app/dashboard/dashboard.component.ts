3a130b81bdd0d008f385b449c57be5c61a9eb242
Ksenia Abakumova <k.s.abakumova@gmail.com>
2018-04-19 19:15:02 +0500

    + | dashboard | new structure ready

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,7 +12,7 @@ import HeatmapOverlay = require('leaflet-heatmap/leaflet-heatmap');
 export class DashboardComponent implements OnInit {
 
   // Charts
-  public view: any[] = [400, 148];
+  public view: any[] = [400, 175];
   public unitsData = [
     {
       'name': 'Unit price',
