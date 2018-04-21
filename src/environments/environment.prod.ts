e456dade3707defe459cd918366ac8f0b0bd5019
Ksenia Abakumova <k.s.abakumova@gmail.com>
2018-04-21 17:11:36 +0500

    +

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   SOCKET_SERVER_URL: '5.45.120.191:7002',
-  API_URL: 'http://5.45.120.191:7000/api/',
+  API_URL: '/api/',
   API_VERSION: 'v1'
 };
